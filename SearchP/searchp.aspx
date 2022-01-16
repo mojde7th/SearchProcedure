86495cce50c94cd66e3e771360cc3a01fb8ed917
mojde7th <mojde7th@gmail.com>
2022-01-16 10:40:01 +0330

    Add project files.

---
+++ SearchP/searchp.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="searchp.aspx.cs" Inherits="SearchP.searchp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>
+<h1>جستجو</h1>
+                <hr />
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                <asp:Button ID="Button1" runat="server" Text="جستجو" OnClick="Button1_Click" /> 
+                <hr />
+                <asp:GridView ID="GridView1" runat="server" EmptyDataText="رکورد یافت نشد"></asp:GridView>
+            </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SearchP/searchp.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace SearchP
+{
+    public partial class searchp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            SqlCommand sqlcomm = new SqlCommand("searchdate",sqlconn);
+            sqlcomm.CommandType = CommandType.StoredProcedure;
+            sqlcomm.Parameters.AddWithValue("@YYMM", TextBox1.Text);
+            sqlconn.Open();
+            GridView1.DataSource = sqlcomm.ExecuteReader();
+            GridView1.DataBind();
+            sqlconn.Close();
+
+        }
+    }
+}
